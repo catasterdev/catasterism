7c3aa90e7ea4ee205e8a1733c58d20d03dc76fd3
Seláf Szabolcs <selu@selu.org>
2010-11-20 16:32:14 +0100

    rails 3.0.3 with pg

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Catasterism::Application.config.secret_token = 'b238214e5bf2e7c8a475be78defef3daf81f9449b05916686121955ae17cb957ca545a5e91234ebef21f514c6abce94406e097418061c7fdf340dee5f88ca1f0'
